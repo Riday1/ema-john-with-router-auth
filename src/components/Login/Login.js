465c901be57c4b0b1c7d0ce9458c7cc27d85a4a7
Riday1 <rrriday100@gmail.com>
2023-09-16 11:37:36 +0600

    applied style on our login form

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import './Login.css'
+import { Link } from 'react-router-dom';
 
 const Login = () => {
     return (
-        <div>
-            <h1>This is login</h1>
+        <div className='form-container'>
+            <h1 className='form-title'>Login</h1>
+            <form >
+                <div className='form-control'>
+                    <label htmlFor="email">Email</label>
+                    <input type="email" name="email" id="" required />
+                </div>
+                <div className='form-control'>
+                    <label htmlFor="passowrd">Passowrd</label>
+                    <input type="passowrd" name="passowrd" id="" required />
+                </div>
+                <input className='btn-submit' type="submit" value="Login" />
+            </form>
+            <p className='create-account'>New to ema john <Link className='' to='/signup'>Create a New Account</Link></p>
         </div>
     );
 };
